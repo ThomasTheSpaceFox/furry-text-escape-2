4a975c325b255185d67d3025d356e6d936575904
thomasthespacefox <teleathers22@gmail.com>
2016-05-26 20:17:26 -0400

    improve main menu

--- TE2.py
+++ TE2.py
@@ -3,32 +3,40 @@
 # Furry Text Escape 2 main script
 gamevers = ('v1.0')
 n = ('null')
+tprint1 = ('1')
 while n.strip()!="3":
-	t = open('./art/title1.TCR', 'r')
-	tcr_contents = t.read()
-	print (chr(27) + "[2J" + chr(27) + "[H" + tcr_contents)
-	t.close()
-	print (
-	'''Furry Text Escape II
+	if tprint1==('1'):
+		t = open('./art/title1.TCR', 'r')
+		tcr_contents = t.read()
+		print (chr(27) + "[2J" + chr(27) + "[H" + tcr_contents)
+		t.close()
+		tprint1=('0')
+		
+		print (
+		'''Furry Text Escape II
 (c) 2015-2016 Thomas Leathers
 '''
-	)
-	print (
-	'''Choose number:
+		)
+		print (
+		'''Choose number:
 1: Begin game
 2: Credits
 3: quit'''
-	)
+		)
+	
 	n = raw_input('choose number from the list above:')
+	print(chr(27) + "[2A")
 	if n=="1":
 		print(chr(27) + "[2J" + chr(27) + "[H")
 		execfile("CINA1-OPEN.py")
 		print(chr(27) + "[2J" + chr(27) + "[H")
+		tprint1 = ('1')
 	
 	if n=="2":
 		print(chr(27) + "[2J" + chr(27) + "[H")
 		execfile("CREDITS.py")
 		print(chr(27) + "[2J" + chr(27) + "[H")
+		tprint1 = ('1')
 	
 t.close()
 
